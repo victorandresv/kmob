74f7df74ec8df571d1993986bcb342f8fd75e2ec
Victor <victorandresv@gmail.com>
2013-06-18 12:57:47 -0400

    Configuración básica de Framework

---
+++ js/app.js
@@ -0,0 +1,28 @@
+window.Application5 = $.extend(true, window.Application5, {
+    "config": {
+        "defaultLayout": "slideout",
+        "navigation": [
+            {
+                title: "Calendario",
+                action: "#Index",
+                icon: "event"
+            },
+            {
+                title: "Acerca de",
+                action: "#About",
+                icon: "info"
+            }
+        ]
+    }
+});
+jQuery(function() {
+    demoApplication = Application5.app = new DevExpress.framework.html.HtmlApplication({
+        ns: Application5,
+        viewPortNode: document.getElementById("viewPort"),
+        defaultLayout: Application5.config.defaultLayout,
+        navigation: Application5.config.navigation
+    });
+
+    Application5.app.router.register(":view/:id", {view: "Index", id: undefined});
+    Application5.app.navigate();
+});
