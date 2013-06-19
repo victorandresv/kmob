9980726e8ac95ef6ac60f5ee272e3428d96127a4
Victor <victorandresv@gmail.com>
2013-06-19 16:02:56 -0400

    Pop up Diario Mural

--- js/diario-mural.js
+++ js/diario-mural.js
@@ -1,14 +1,21 @@
-employees = [
+DiarioMuralFechas = [
     {
         key: 'Semana 16 de Junio hasta 22 de Junio',
         items: [
-            {name: "Lunes", hired: '16 <span class="number-red">5</span>'},
-            {name: "Martes", hired: '17 <span class="number-red">2</span>'},
-            {name: "Miércoles", hired: '18 <span class="number-red">4</span>'},
-            {name: "Jueves", hired: '19 <span class="number-red">2</span>'},
-            {name: "Viernes", hired: '20'},
-            {name: "Sábado", hired: '21 <span class="number-red">1</span>'},
-            {name: "Domingo", hired: '22'},
+            {name: "Lunes", day:'16', notify: '<span class="number-red">5</span>', date:'2013-06-16'},
+            {name: "Martes", day:'17', notify: '<span class="number-red">2</span>', date:'2013-06-17'},
+            {name: "Miércoles", day:'18', notify: '<span class="number-red">4</span>', date:'2013-06-18'},
+            {name: "Jueves", day:'19', notify: '<span class="number-red">2</span>', date:'2013-06-19'},
+            {name: "Viernes", day:'20', notify: '<span class="number-red">6</span>', date:'2013-06-20'},
+            {name: "Sábado", day:'21', notify: '<span class="number-red">1</span>', date:'2013-06-21'},
+            {name: "Domingo", day:'22', notify: '', date:'2013-06-22'},
         ]
     }
 ];
+DiarioMuralDetalle = ko.observable(false);
+diarioMuralItemOnClick = function(e){
+    items = e.itemData;
+    DiarioMuralDetalle(true);
+    jQuery('#dx_title_popup').html(items.name+' '+items.day+' de Junio');
+}
+
